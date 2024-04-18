85088697faaef00b54892f101f92fb6fab5a6894
shauncommits <shaunmbolompo426@gmail.com>
2024-04-18 16:05:39 +0200

    Edit the updateEmployee method of IEmployeeFactory.cs interface

--- EmployeeWebApp/Models/IEmployeeFactory.cs
+++ EmployeeWebApp/Models/IEmployeeFactory.cs
@@ -4,7 +4,7 @@ public interface IEmployeeFactory
 {
     Employee GetEmployeeById(int id);
     void AddEmployee(Employee employee);
-    void UpdateEmployee(int id);
+    void UpdateEmployee(int id, Employee employee);
     void DeleteEmployee(int id);
     IEnumerable<Employee> GetEmployeeList();
 }
