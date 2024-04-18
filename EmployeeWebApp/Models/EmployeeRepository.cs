e17fac0fcc864ff990de268eb0236304a26dab72
shauncommits <shaunmbolompo426@gmail.com>
2024-04-18 16:04:42 +0200

    Edit EmployeeRepository.cs to use EmployeeDbContext to get data from the database

--- EmployeeWebApp/Models/EmployeeRepository.cs
+++ EmployeeWebApp/Models/EmployeeRepository.cs
@@ -3,34 +3,47 @@ namespace EmployeeWebApp.Models;
 public class EmployeeRepository: IEmployeeFactory
 {
     private List<Employee> _employeeList;
+    private readonly EmployeeDbContext _dbContext;
 
-    public EmployeeRepository()
+    public EmployeeRepository(EmployeeDbContext dbContext)
     {
-        
+        _dbContext = dbContext;
     }
 
     public Employee GetEmployeeById(int id)
     {
-        throw new NotImplementedException();
+        return _dbContext.Employees.Find(id);
     }
 
     public void AddEmployee(Employee employee)
     {
-        throw new NotImplementedException();
+        _dbContext.Employees.Add(employee);
+        _dbContext.SaveChanges();
     }
 
-    public void UpdateEmployee(int id)
+    public void UpdateEmployee(int id, Employee updatedEmployee)
     {
-        throw new NotImplementedException();
+        var employee = _dbContext.Employees.Find(id);
+       
+        employee.Name = updatedEmployee.Name;
+        employee.Department = updatedEmployee.Department;
+        employee.Email = updatedEmployee.Email;
+        
+        _dbContext.SaveChanges();
     }
 
     public void DeleteEmployee(int id)
     {
-        throw new NotImplementedException();
+        var employee = _dbContext.Employees.Find(id);
+        if (employee != null)
+        {
+            _dbContext.Employees.Remove(employee);
+            _dbContext.SaveChanges();
+        }
     }
 
     public IEnumerable<Employee> GetEmployeeList()
     {
-        throw new NotImplementedException();
+        return _dbContext.Employees.ToList();
     }
 }
