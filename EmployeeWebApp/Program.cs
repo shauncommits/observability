21d6078cfbd709f8047a0cfaf533fc93f0f3d41a
shauncommits <shaunmbolompo426@gmail.com>
2024-04-18 13:31:52 +0200

    Add the DB connection string

--- EmployeeWebApp/Program.cs
+++ EmployeeWebApp/Program.cs
@@ -9,13 +9,19 @@ var configBuilder = new ConfigurationBuilder()
 
 string username = configBuilder["profiles:Development:environmentVariables:DB_USERNAME"];
 string password = configBuilder["profiles:Development:environmentVariables:DB_PASSWORD"];
+string dbName = configBuilder["profiles:Development:environmentVariables:DB_NAME"];
 
 var appSettingsBuilder = new ConfigurationBuilder()
     .AddJsonFile("appsettings.json")
     .Build();
+string _connStr = @"
+  Server=localhost,1433;
+  Database=EmployeeDB;
+  User Id=sa;
+  Password=Password_123";
 
-string connectionString = appSettingsBuilder["ConnectionStrings:DefaultConnection"]
-    .Replace("<YourDatabaseName>", "your-database-name")
+string connectionString = appSettingsBuilder["ConnectionStrings:"+_connStr]
+    .Replace("${DB_NAME}", dbName)
     .Replace("${DB_USERNAME}", username)
     .Replace("${DB_PASSWORD}", password);
 
