4138ebe10ea52cf752badb6de2fcea0607bf53f1
shauncommits <shaunmbolompo426@gmail.com>
2024-04-19 09:04:40 +0200

    Add routes for the razor pages I added

--- EmployeeWebApp/Controllers/EmployeeController.cs
+++ EmployeeWebApp/Controllers/EmployeeController.cs
@@ -19,6 +19,59 @@ public class EmployeeController : Controller
     {
         return View(_employeeFactory.GetEmployeeList());
     }
+    public IActionResult Add()
+    {
+        return View();
+    }
+    
+    [HttpPost]
+    public IActionResult Add(Employee employee)
+    {
+        if (ModelState.IsValid)
+        {
+            _employeeFactory.AddEmployee(employee);
+            return RedirectToAction("Index");
+        }
+        return View(employee);
+    }
+    
+    public IActionResult Edit(int id)
+    {
+        var employee = _employeeFactory.GetEmployeeById(id);
+        if (employee == null)
+        {
+            return NotFound();
+        }
+        return View(employee);
+    }
+    
+    [HttpPost]
+    public IActionResult Edit(Employee employee)
+    {
+        if (ModelState.IsValid)
+        {
+            _employeeFactory.UpdateEmployee(employee);
+            return RedirectToAction("Index");
+        }
+        return View(employee);
+    }
+    
+    public IActionResult Delete(int id)
+    {
+        var employee = _employeeFactory.GetEmployeeById(id);
+        if (employee == null)
+        {
+            return NotFound();
+        }
+        return View(employee);
+    }
+    
+    [HttpPost]
+    public IActionResult DeleteConfirmed(int id)
+    {
+        _employeeFactory.DeleteEmployee(id);
+        return RedirectToAction("Index");
+    }
 
     public IActionResult Privacy()
     {
