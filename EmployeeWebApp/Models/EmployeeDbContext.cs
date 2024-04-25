1386c4c84c2c2e819415ac0ba5f95c8f41ab9153
shauncommits <shaunmbolompo426@gmail.com>
2024-04-25 12:03:34 +0200

    Make the DBContext to pick Id_Number as the key for the data

--- EmployeeWebApp/Models/EmployeeDbContext.cs
+++ EmployeeWebApp/Models/EmployeeDbContext.cs
@@ -8,6 +8,9 @@ public class EmployeeDbContext: DbContext
     {
         
     }
-    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Employee>().HasKey(e => e.Id_Number);
+    }
     public DbSet<Employee> Employees { get; set; }
 }
