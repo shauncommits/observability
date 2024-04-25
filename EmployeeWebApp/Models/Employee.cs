c09a708c3a738e95475d003c3bb0949f73b36df0
shauncommits <shaunmbolompo426@gmail.com>
2024-04-25 11:59:38 +0200

    Edit the employee Id to differ from the one of Elastic search

--- EmployeeWebApp/Models/Employee.cs
+++ EmployeeWebApp/Models/Employee.cs
@@ -2,7 +2,7 @@ namespace EmployeeWebApp.Models;
 
 public class Employee
 {
-    public int Id { get; set; }
+    public long Id_Number { get; set; }
     public string Name { get; set; }
     public string Email { get; set; }
     public Department? Department { get; set; }
