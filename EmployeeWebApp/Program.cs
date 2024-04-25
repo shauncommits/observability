4722b0f17411dacdbae62c10726bb95da3e35a23
shauncommits <shaunmbolompo426@gmail.com>
2024-04-25 12:08:07 +0200

    Add Open Telemetry configuration

--- EmployeeWebApp/Program.cs
+++ EmployeeWebApp/Program.cs
@@ -1,6 +1,9 @@
 ﻿using EmployeeWebApp.Models;
 using Microsoft.EntityFrameworkCore;
 using Nest;
+using OpenTelemetry;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,32 +11,57 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddScoped<IEmployeeFactory, EmployeeRepository>();
 
-string currentDirectory = Directory.GetCurrentDirectory();
-string relativeLaunchSettingsPath = Path.Combine(currentDirectory,"Properties" ,"launchSettings.json");
-
-var configBuilder = new ConfigurationBuilder()
-    .AddJsonFile(relativeLaunchSettingsPath)
-    .Build();
-
-string username = configBuilder["profiles:http:environmentVariables:DB_USERNAME"];
-string password = configBuilder["profiles:http:environmentVariables:DB_PASSWORD"];
-string dbName = configBuilder["profiles:http:environmentVariables:DB_NAME"];
+// string currentDirectory = Directory.GetCurrentDirectory();
+// string relativeLaunchSettingsPath = Path.Combine(currentDirectory,"Properties" ,"launchSettings.json");
+//
+// var configBuilder = new ConfigurationBuilder()
+//     .AddJsonFile(relativeLaunchSettingsPath)
+//     .Build();
+//
+// string username = configBuilder["profiles:http:environmentVariables:DB_USERNAME"];
+// string password = configBuilder["profiles:http:environmentVariables:DB_PASSWORD"];
+// string dbName = configBuilder["profiles:http:environmentVariables:DB_NAME"];
 
 var appSettingsBuilder = new ConfigurationBuilder()
     .AddJsonFile("appsettings.json")
     .Build();
 
 string connectionString = appSettingsBuilder["ConnectionStrings:DefaultConnection"]
-    .Replace("${DB_NAME}", dbName)
-    .Replace("${DB_USERNAME}", username)
-    .Replace("${DB_PASSWORD}", password);
+    .Replace("${DB_NAME}", "EmployeeDB")
+    .Replace("${DB_USERNAME}", "sa")
+    .Replace("${DB_PASSWORD}", "Password_123");
 
 builder.Services.AddDbContext<EmployeeDbContext>(options =>
     options.UseSqlServer(connectionString));
 
 // Create an instance of the Elasticsearch client
-var settings = new ConnectionSettings(new Uri("http://localhost:9200")).DefaultIndex("employee_web-app");
+var settings = new ConnectionSettings(new Uri("http://elasticsearch:9200")).DefaultIndex("employee_web_app");
 builder.Services.AddSingleton<IElasticClient>(new ElasticClient(settings));
+
+
+// Define some important constants and the activity source.
+// These can come from a config file, constants file, etc.
+var serviceName = "EmployeeOTELServiceFromWebApp";
+var serviceVersion = "1.0.0";
+
+// Configure important OpenTelemetry settings, the console exporter
+builder.Services.AddOpenTelemetry()
+    .WithTracing(b =>
+    {
+        b
+            .AddSource(serviceName)
+            .ConfigureResource(resource =>
+                resource.AddService(
+                    serviceName: serviceName,
+                    serviceVersion: serviceVersion))
+            .AddAspNetCoreInstrumentation()
+            .AddJaegerExporter(exporterOptions =>
+            {
+                exporterOptions.ExportProcessorType = ExportProcessorType.Batch;
+            }); // Adding jaeger exporter
+    });
+
+
 
 var app = builder.Build();
 
